6eb8237aa2ebf2fa76eb201c00d7fd9aca02a941
Денис <denisboarsci@gmail.com>
2024-11-20 00:43:42 +0500

    done 20.11.24 Фикс кода по обработке значений. todo: Доработать и создать словарь {Имя:Оценка}

--- average score of each student automatic calculation.py
+++ average score of each student automatic calculation.py
@@ -1,15 +1,17 @@
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+students_grades = []
 students_sorted = sorted(students)
-students_sorted = list(students_sorted)
-students_sorted_nuw = []
-students_sorted_nuw[0] = (students_sorted[0]+':',(sum(grades[0])/len(grades[0])))
-students_sorted_nuw[1] = (students_sorted[1]+':',(sum(grades[1])/len(grades[1])))
-students_sorted_nuw[2] = (students_sorted[2]+':',(sum(grades[2])/len(grades[2])))
-students_sorted_nuw[3] = (students_sorted[3]+':',(sum(grades[3])/len(grades[3])))
-students_sorted_nuw[4] = (students_sorted[4]+':',(sum(grades[4])/len(grades[4])))
+#students_sorted = list(students_sorted)
+print(students_sorted)
+students_grades.append((students_sorted[0],(sum(grades[0])/len(grades[0]))))
+students_grades.append((students_sorted[1],(sum(grades[1])/len(grades[1]))))
+students_grades.append((students_sorted[2],(sum(grades[2])/len(grades[2]))))
+students_grades.append((students_sorted[3],(sum(grades[3])/len(grades[3]))))
+students_grades.append((students_sorted[4],(sum(grades[4])/len(grades[4]))))
+print(students_grades[0])
 
-print(students_sorted_nuw)
+
 
 
 
